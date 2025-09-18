98e7e7fde211b259eac145c4998128780425e748
Odizinne <flora.aubry@proton.me>
2025-09-18 14:23:15 +0200

    custom font

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QFontDatabase>
 
 int main(int argc, char *argv[])
 {
+    qputenv("QT_QUICK_CONTROLS_MATERIAL_VARIANT", "Dense");
     QGuiApplication app(argc, argv);
 
+    qint32 fontId = QFontDatabase::addApplicationFont(":/fonts/PerfectLemonade.otf");
+    QStringList fontList = QFontDatabase::applicationFontFamilies(fontId);
+    QString family = fontList.first();
+
+    app.setFont(QFont(family));
+    app.setOrganizationName("Odizinne");
+    app.setApplicationName("Flowibi");
     QQmlApplicationEngine engine;
     QObject::connect(
         &engine,
